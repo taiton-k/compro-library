6785f57fb642c0c04bda78e90da001d618ab29dd
taiton-k <taitanknowcat@cock.li>
2022-06-28 09:35:24 +0900

    update example

--- example/sieve_of_eratosthenes.cpp
+++ example/sieve_of_eratosthenes.cpp
@@ -8,10 +8,17 @@ int main () {
 
         compro::SieveOfEratosthenes sieve(100);
 
+        std::cout << std::boolalpha <<
+                sieve.isPrime(2) << std::endl << // true
+                sieve.isPrime(7) << std::endl << // true
+                sieve.isPrime(4) << std::endl << // false
+                sieve.isPrime(9) << std::endl;   // false
+
         std::vector<int> primes;
         sieve.makeList(std::back_inserter(primes));
 
         for (int prime : primes) {
                 std::cout << prime << ' ';
         }
+        std::cout << std::endl;
 }
